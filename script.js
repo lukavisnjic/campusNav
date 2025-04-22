3ffbebc59433c902230c7edee094d2ae9885da96
Luka Visnjic <luka_visnjic@mines.edu>
2025-04-21 21:31:11 -0600

    integration

--- script.js
+++ script.js
@@ -91,3 +91,130 @@ function getRoute() {
     }
   );
 }
+
+var input = document.getElementById("roomInput");
+
+input.addEventListener("keypress", function(event) {
+if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("roomIn").click();
+    }
+});
+
+
+// FLOOR PLAN HIGHLIGHTS
+function highlightRoom() {
+    let input = document.getElementById("roomInput").value.trim().toUpperCase();
+    console.log(input);
+    // Define room mapping
+    let rooms = {
+        "183": "HH183",
+        "181": "HH181",
+        "178": "HH178",
+        "178A": "HH178A",
+        "178B": "HH178B",
+        "178C": "HH178C",
+        "177": "HH177",
+        "176": "HH176",
+        "176B": "HH176B",
+        "176C": "HH176C",
+        "176D": "HH176D",
+        "176F": "HH176F",
+        "175": "HH175",
+        "174": "HH174",
+        "173": "HH173",
+        "172": "HH172",
+        "171": "HH171",
+        "170": "HH170",
+        "162": "HH162",
+        "161": "HH161",
+        "160": "HH160",
+        "164": "HH164",
+        "151": "HH151",
+        "151A": "HH151A",
+        "150": "HH150",
+        "139": "HH139",
+        "137": "HH137",
+        "133": "HH133",
+        "132": "HH132",
+        "131A": "HH131A",
+        "131": "HH31",
+        "129A": "HH129A",
+        "129B": "HH129B",
+        "128": "HH128",
+        "128A": "HH128A",
+        "128B": "HH128B",
+        "126": "HH126",
+        "125B": "HH125B",
+        "125A": "HH125A",
+        "125C": "HH125C",
+        "125": "HH125",
+        "124": "HH124",
+        "122": "HH122",
+        "120": "HH120",
+        "123": "HH123",
+        "123A": "HH123A",
+        "123B": "HH123B",
+        "118": "HH118",
+        "121": "HH121",
+        "121B": "HH121B",
+        "121A": "HH121A",
+        "112": "HH112",
+        "114": "HH114",
+        "116": "HH116",
+        "119": "HH119",
+        "106": "HH106",
+        "104": "HH104",
+        "102": "HH102",
+        "115": "HH115",
+        "113": "HH113",
+        "111A": "HH111A",
+        "111": "HH111",
+        "109": "HH109",
+        "108": "HH108",
+        "108A": "HH108A",
+        "107": "HH107",
+        "136": "HH136",
+        "105": "HH105",
+        "103": "HH103",
+        "102": "HH102",
+        "101": "HH101",
+        "140": "HH140",
+        "141": "HH141",
+        "144": "HH144",
+        "149": "HH149",
+        "179": "HH179"
+    };
+
+    // Hide all rooms first
+    document.querySelectorAll('.highlight').forEach(room => room.classList.add('hidden'));
+    
+    if (input == "") {
+        emptyPopup();
+    }
+    else if (rooms[input]) {
+
+        document.getElementById(rooms[input]).classList.remove('hidden');
+
+
+    } else {
+            showPopup();
+
+    }
+}
+
+function emptyPopup() {
+    document.getElementById("emptyPopup").style.display = "block";
+}
+
+function closeEmptyPopup() {
+    document.getElementById("emptyPopup").style.display = "none";
+}
+
+function showPopup() {
+    document.getElementById("errorPopup").style.display = "block";
+}
+
+function closePopup() {
+    document.getElementById("errorPopup").style.display = "none";
+}
